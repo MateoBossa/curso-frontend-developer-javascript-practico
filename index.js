dcf51df35a67891119e22efc3886d8150aa89913
Clevenot909 <mateobossa909@gmail.com>
2022-09-13 18:34:59 -0500

    Nuevos cambios

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const menuLeft = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -11,20 +11,20 @@ menuLeft.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
     
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = !aside.classList.contains('inactive');
+    const isAsideClosed = !shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -37,24 +37,39 @@ function toggleCarritoAside() {
         mobileMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
 productList.push({
     name: 'Bike',
-    price: 120,
+    price: '340.000',
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 productList.push({
     name: 'Screen',
-    price: 240,
+    price: '240.000',
     image: 'https://images.pexels.com/photos/704555/pexels-photo-704555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 });
 productList.push({
     name: 'Phone',
-    price: 530,
+    price: '2.340.300',
     image: 'https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=600',
+});
+productList.push({
+    name: 'Lacoste Essential',
+    price: '650.000',
+    image: 'https://images.pexels.com/photos/989962/pexels-photo-989962.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'Creed Aventus',
+    price: '1.300.000',
+    image: 'https://cdn.shopify.com/s/files/1/0271/3298/5441/products/1110042_AV_100ml.jpg?v=1614177112'
+});
+productList.push({
+    name: 'Light Blue',
+    price: '435.000',
+    image: 'https://falabella.scene7.com/is/image/FalabellaCO/1741365_1?wid=800&hei=800&qlt=70'
 });
 
 function renderProducts(array) {
